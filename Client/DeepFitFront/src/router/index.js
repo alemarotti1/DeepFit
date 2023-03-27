905f00ed18614b80c26b215e3f3f4bf68046cd96
Flávio Lins <flmn@cin.ufpe.br>
2023-03-27 16:00:58 -0300

    starting vue project

---
+++ Client/DeepFitFront/src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const Home = () => import('../views/Home/Home.vue')
+const About = () => import('../views/AboutView.vue')
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView
+    // },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
+    }
+  ]
+})
+
+export default router
---
+++ Client/DeepFitFront/src/views/Home/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="11">
+        <span>Bem vindo</span>
+        <h1>{{ userName }}</h1> 
+        
+      </v-col>
+      <v-col cols="1">
+        <v-icon icon="mdi-account-outline">
+
+        </v-icon>
+      </v-col>
+
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      userName: 'João Gabriel' 
+    }
+  }
+}
+</script>
+<style>
+</style>
